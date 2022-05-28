dce71e650d6b5901725429662ce1b21b8d981d9b
Caio Vinicius <caioviniciuscv18@gmail.com>
2022-05-28 07:41:31 -0300

    Fixing errors and fixing Warning

--- src/main/java/com/tenniscourts/TennisCourtApplication.java
+++ src/main/java/com/tenniscourts/TennisCourtApplication.java
@@ -1,8 +1,10 @@
 package com.tenniscourts;
 
+import com.tenniscourts.guests.Guest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
